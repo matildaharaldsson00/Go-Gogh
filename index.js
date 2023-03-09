c2db5676dabd95b4510f85da4db49109501b2926
matildaharaldsson00 <97900355+matildaharaldsson00@users.noreply.github.com>
2023-03-09 18:23:33 +0100

    lösning?
    ett förslag, fungerar inte helt men kanske en början?

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
 function page2GoGogh() {
-    window.location.href = "page2.html"
+    window.location.href = "page2.html";
 }
+
+const nextHouseBut = document.getElementById("nextHouseBut");
+
+nextHouseBut.addEventListener("click", function () {
+    const firstArtwork = document.getElementById("firstArtwork");
+    const secondArtwork = document.getElementById("secondArtwork");
+
+    firstArtwork.style.transform = "translateX(-100%)";
+    secondArtwork.style.transform = "translateX(0%)";
+
+    secondArtwork.addEventListener("transitionend", function () {
+        firstArtwork.style.display = "none";
+        secondArtwork.style.display = "block";
+    });
+});
